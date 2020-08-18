f21fc8ba75693d15c434a08727c931733decf43b
Elad <elad725@gmail.com>
2020-08-18 19:25:01 +0300

    more js advance, setting up cart UI

--- app.js
+++ app.js
@@ -16,11 +16,25 @@ let cart = [];
 
 // קלאס שאחראי על השגת המוצרים
 class Products {
+
+    //מגדירים איך המוצרים מהג'ייסון נותנים את הדאטה שלהם
     async getProducts() {
         try {
             let result = await fetch("products.json");
             let data = await result.json();
-            return data;
+
+            let products = data.items;
+            products = products.map(item =>{
+              //תביא לי את שתי התכונות האלה של החפצים מהמערך
+                const {title,price} = item.fields;
+             //תביא לי את האיידי של החפצים מהמערך
+                const {id} = item.sys;
+            //תביא לי את התמונה של החפצים מהמערך
+                const image = item.fields.image.fields.file.url;
+                return {title,price,id,image}
+                //it's like " title = title, price = price" and so on
+             })
+             return products
         } catch (error) {
             console.log(error);
         }
@@ -28,8 +42,31 @@ class Products {
 }
 
 //קלאס שאחראי על הצגת המוצרים
+//כל מה שמופיע על המסך מסודר פה
 class UI {
+displayProducts(products) {
+    let result = '';
+    products.forEach(product => {
+        result += `
+        <article class="product">
+        <div class="img-container">
+            <img 
+            src=${product.image}
+            alt=${product.title}
+            class="product-img" />
+            <button class="bag-btn" data-id=${product.id}>
+                <i class="fas fa-shopping-cart"></i>
+                add to bag
+            </button>
 
+        </div>
+        <h3>${product.title}</h3>
+        <h4>₪${product.price}</h4>
+        </article>
+            <!-- end of single product -->
+        `
+     })
+    }
 }
 
 //אחסון מקומי - local storage
@@ -42,5 +79,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const products = new Products();
 
     //get all products
-    products.getProducts().then(data => console.log(data));
+    products.getProducts().then(products => ui.displayProducts(products));
 })
